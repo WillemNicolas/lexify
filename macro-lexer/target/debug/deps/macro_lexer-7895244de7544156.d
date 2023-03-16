ce3881f6b413bb5c1fc2eaee6d0ecbdc486370e1
WillemNicolas <darwil.nicolas@gmail.com>
2023-03-16 22:42:16 +0100

    cargo new

---
+++ macro-lexer/target/debug/deps/macro_lexer-7895244de7544156.d
@@ -0,0 +1,5 @@
+/home/willem/Workspace/rust/macro-lexer/macro-lexer/target/debug/deps/macro_lexer-7895244de7544156: src/main.rs
+
+/home/willem/Workspace/rust/macro-lexer/macro-lexer/target/debug/deps/macro_lexer-7895244de7544156.d: src/main.rs
+
+src/main.rs:
---
+++ macro-lexer/target/debug/macro-lexer.d
@@ -0,0 +1,1 @@
+/home/willem/Workspace/rust/macro-lexer/macro-lexer/target/debug/macro-lexer: /home/willem/Workspace/rust/macro-lexer/macro-lexer/src/main.rs
